65a4578cf15e46e48ad232ee71b5da55f962d805
Guilherme Defreitas <guilherme.defreitas@gmail.com>
2017-04-03 16:01:39 -0300

    Add menu scene

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+	private GvrViewer gvrMain;
+
+	void Awake(){
+		this.gvrMain = GameObject.FindGameObjectWithTag("GvrViewerMain").GetComponent<GvrViewer>();
+	}
+
+	IEnumerator StartGameAsync() {
+        AsyncOperation async = Application.LoadLevelAsync("Scene1");
+        yield return async;
+        Debug.Log("Loading complete");
+    }
+	public void StartGame(){
+		StartCoroutine("StartGameAsync");
+	}
+
+	public void GoToOptions(){
+
+	}
+
+	public void ToggleVRMode(){
+		Debug.Log("ToggleVR clicked");
+		this.gvrMain.VRModeEnabled = !this.gvrMain.VRModeEnabled;
+	}
+}
